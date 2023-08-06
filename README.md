195ccb7869d0c736b31b46ccb9b07647dfb3abdc
flavienbwk <flavien@berwick.fr>
2023-08-06 11:58:09 +0200

    Added initial schema and explanations

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# azure-spending-limits
-Learn how to set soft and hard limits on Microsoft Azure to prevent unexpected costs.
+# Azure Spending Limits
+
+Minimal configuration to set soft **and hard** limits on Microsoft Azure to avoid overspending with a "Pay-as-you-go" account.
+
+Some examples of why it's useful :
+
+- Avoid unexpected API consumption spending (such as infinite loops)
+- Avoid cost for forgotten VMs or resources
+
+The provided script will delete the services in a resource group with an action group. It won't delete the resource group, which is convenient. Also, this is a last resort solution as it will require you to reconfigure your services after removal.
+
+_This method is probably not the best but is "quick" to setup and just works. You might prefer [rate-limiting](https://techcommunity.microsoft.com/t5/azure-paas-blog/configure-rate-limits-for-different-api-operations-in-azure-api/ba-p/3789108) API calls, or trigger an action group on a [carefully analyzed spending](https://learn.microsoft.com/en-us/answers/questions/931661/how-can-i-find-how-much-per-hour-i-am-being-billed) (spoiler: this is hard)._
+
+## How to set soft and hard limits
+
+Remember that accumulated cost showing in your "Cost Management" console is an estimate only. Real cost may [take time to update](https://learn.microsoft.com/en-us/azure/azure-monitor/usage-estimated-costs) and is generally stabilized one day after a resource is consumed. However, it is possible to set hard limits based on [forecasted cost](https://learn.microsoft.com/en-us/azure/cost-management-billing/finops/capabilities-forecasting).
+
+This schema represents the steps to set soft and hard limits on a resource group :
+
+![Schema of the workflow configured to delete resources when a spending limit is reached.](./delete-resource-workflow-azure.jpg)
---
+++ list-and-delete-resource.py
@@ -0,0 +1,77 @@
+# List and delete resources from a resource group using the Azure API (can be used in runbooks then triggered by Monitor > Alerts > Create action groups when budget is exceeded).
+# It requires an "Application Insights" resource.
+# Configure the CLIENT_ID (Azure App Registration) to be able to read/delete RESOURCE_GROUP's resources in the IAM of the RESOURCE_GROUP.
+#
+# Useful link : https://learn.microsoft.com/en-us/azure/cost-management-billing/manage/cost-management-budget-scenario#create-webhooks-for-the-runbook
+
+import requests
+
+def extract_type_prefix(input_string):
+    if '/' in input_string:
+        return input_string.split('/')[0]
+    return input_string
+
+# Azure app registration details
+TENANT_ID = 'xxxxxxxxxxx' # In Azure AD
+CLIENT_ID = 'xxxxxxxxxxx' # In Azure App Registration
+CLIENT_SECRET = 'xxxxxxxxxxx'
+RESOURCE = 'https://management.azure.com/'
+
+# Azure resource details to be deleted
+SUBSCRIPTION_ID = 'xxxxxxxxxxx'
+RESOURCE_GROUP = 'xxxxxxxxxxx'
+RESOURCE_TYPE = 'accounts'
+
+# Endpoint for acquiring token
+TOKEN_ENDPOINT = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/token"
+
+# Acquire token
+data = {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+    'resource': RESOURCE
+}
+
+token_response = requests.post(TOKEN_ENDPOINT, data=data)
+token = token_response.json().get('access_token')
+
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/json'
+}
+
+RESOURCES_PROVIDER_AND_NAME = []
+LIST_RESOURCES_ENDPOINT = f"https://management.azure.com/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP}/resources?api-version=2021-04-01"
+response = requests.get(LIST_RESOURCES_ENDPOINT, headers=headers)
+if response.status_code == 200:
+    resources = response.json().get('value', [])
+    print("Resources in the resource group:")
+    for resource in resources:
+        RESOURCES_PROVIDER_AND_NAME.append({
+            "provider": extract_type_prefix(resource['type']),
+            "name": resource['name']
+        })
+        print(resource['name'])
+else:
+    print(f"Error retrieving resources. HTTP Status Code: {response.status_code}")
+    print(response.text)
+
+print("\n")
+
+# Delete the resource
+API_VERSION_FROM_PROVIDER = {
+    "Microsoft.Bing": "2020-06-10",
+    "Microsoft.CognitiveServices": "2021-04-30"
+}
+for resource in RESOURCES_PROVIDER_AND_NAME:
+    print(f"{resource['name']}: Deleting...")
+    DELETE_ENDPOINT = f"https://management.azure.com/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP}/providers/{resource['provider']}/{RESOURCE_TYPE}/{resource['name']}?api-version={API_VERSION_FROM_PROVIDER[resource['provider']]}"
+    response = requests.delete(DELETE_ENDPOINT, headers=headers)
+    if response.status_code in [202, 204]:
+        print(f"{resource['name']}: Resource deletion initiated successfully!")
+    elif response.status_code == 200:
+        print(f"{resource['name']}: Resource deleted successfully!")
+    else:
+        print(f"{resource['name']}: Error deleting resource. HTTP Status Code: {response.status_code}")
+        print(response.text)
